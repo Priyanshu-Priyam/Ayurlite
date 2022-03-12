83beea29ddd46d0730aa58c8922b8d67fcdc72e6
Priyanshu-Priyam <79155933+Priyanshu-Priyam@users.noreply.github.com>
2022-03-12 17:30:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import model
+from model import classifier
+from model import extract_features
+from model import DecisionTreeClassifier
+from model import X_test
+from model import X_train
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"] = "static/"
+
+@app.route('/')
+def upload_file():
+    return render_template('index.html')
+
+
+@app.route('/display', methods = ['GET', 'POST'])
+
+
+
+def save_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        
+        f.save(app.config['UPLOAD_FOLDER'] + filename)
+
+        file = open(app.config['UPLOAD_FOLDER'] + filename,"r")
+        #content = file.read()
+
+        import numpy as np
+        #fn=input("Enter the name of the file")ß
+        x_user = extract_features(filename)
+    
+        prediction = classifier.predict(x_user)
+
+        content= prediction 
+
+
+    return render_template('content.html', content=content) 
+
+
+
+"""
+
+
+ 
+"""
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug = True)
+
+   
