3cf9dca01b4cc6eaad3c703e51148e8bafd9b1c2
Priyanshu Priyam <79155933+Priyanshu-Priyam@users.noreply.github.com>
2022-04-26 16:44:27 +0530

    Add files via upload

---
+++ s3_demo.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import pickle
+from model import extract_features
+import os
+from flask import Flask, render_template, request, redirect, send_file
+import boto3
+
+
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, object_name)
+    return response
+
+
+
+    return response
+
+def list_files(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3 = boto3.client('s3')
+    contents = []
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+
+    return contents
